dd51a4177506759d17118aa37cf39e9357872142
Kamal Ch <kamal.ch@live.ca>
2024-11-10 02:20:41 -0500

    random recipe get

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -3,7 +3,11 @@ export const getRandomMeal = async (req, res) => {
         const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
         const data = await response.json();
         const meal = data.meals[0];
-        res.render('recipe', { meal });
+
+        // Split instructions into individual steps
+        const instructions = meal.strInstructions.split('\r\n').filter(step => step.trim() !== "");
+
+        res.render('recipe', { meal, instructions });
     } catch (error) {
         console.error('Error fetching data:', error);
         res.send('Error fetching data');
